8835fe488a016f6d99fd365eac6ba9ad83d93b36
DC <43692686+excaliburDC@users.noreply.github.com>
2020-02-02 11:45:00 +0530

    Added Enemy and updated levels

---
+++ Assets/Scripts/EnemyStroll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStroll : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool moveRight;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    void EnemyMove()
+    {
+        if(moveRight)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="PlatformColliders")
+        {
+            Debug.Log("Enemy entered trigger");
+            new WaitForSeconds(1f);
+            if (moveRight)
+                moveRight = false;
+            else
+                moveRight = true;
+        }
+    }
+}
